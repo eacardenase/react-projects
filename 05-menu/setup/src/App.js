a5352b32f7c13c31907198163dab4a14bc40c555
Edwin Alexander Cárdenas <eacardenase@unal.edu.co>
2021-12-13 11:13:09 -0500

    menu project

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -1,10 +1,40 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
+const uniqueCategories = [
+    "all",
+    ...new Set(items.map((item) => item.category)),
+];
 
 function App() {
-  return <h2>menu project setup</h2>;
+    const [menuItems, setMenuItems] = useState(items);
+    const [categories, setCategories] = useState(uniqueCategories);
+
+    const filterItems = (category) => {
+        if (category === "all") {
+            setMenuItems(items);
+            return;
+        }
+        const newItems = items.filter((item) => {
+            return item.category === category;
+        });
+        setMenuItems(newItems);
+    };
+
+    return (
+        <main>
+            <section className="menu section">
+                <div className="title">
+                    <h2>our menu</h2>
+                    <div className="underline"></div>
+                </div>
+                <Categories filterItems={filterItems} categories={categories} />
+                <Menu items={menuItems} />
+            </section>
+        </main>
+    );
 }
 
 export default App;
