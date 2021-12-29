bb60e64d165459feb4b41c70ac12d8a6e5822006
Edwin Alexander Cárdenas <eacardenase@unal.edu.co>
2021-12-29 12:58:43 -0500

    cocktails project context-api, useState, useEffect, useRef, useCallback hooks

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-  return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    return (
+        <section className="section about-section">
+            <h1 className="section-title">about us</h1>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi
+                voluptate omnis nesciunt tempore hic. Maiores libero voluptate
+                consequuntur omnis provident asperiores dolores, vel fugiat,
+                cumque aliquid dolor enim mollitia incidunt assumenda, quam
+                voluptatum accusantium sint exercitationem. Maiores iusto
+                nostrum obcaecati!
+            </p>
+        </section>
+    );
+};
 
-export default About
+export default About;
